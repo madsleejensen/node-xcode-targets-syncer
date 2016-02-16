2fcc883ed05845b4da89575b849c3ab90a29dd7e
Mads Lee Jensen <madsleejensen@gmail.com>
2016-02-16 16:13:11 +0100

    search for projects, and added option to pick found projects

--- index.js
+++ index.js
@@ -1,58 +1,95 @@
-var xcode = require("xcode");
-var fs = require("fs");
-var extend = require("extend");
-var path = require("path");
+const xcode = require("xcode");
+const fs = require("fs");
+const extend = require("extend");
+const path = require("path");
+const glob = require("glob");
+const readline = require("readline");
+const chalk = require("chalk");
 
-var filePath = 'test/TargetSync/TargetSync.xcodeproj/project.pbxproj';
-var project = xcode.project(filePath);
-project.parse(function(error, data) {
-
-    function BuildPhaseSyncer() {
-        var buildFileSection = project.pbxBuildFileSection();
-        var buildFileReferenceSection = project.pbxFileReferenceSection();
-
-        function getBuildPhaseByTargetName(targetName) {
-            var key = project.findTargetKey(targetName);
-            var buildPhase = project.pbxSourcesBuildPhaseObj(key);
-
-            return buildPhase;
-        }
-
-        this.sync = function(sourceTargetName, destinationTargetName) {
-            var sourceBuildPhase = getBuildPhaseByTargetName(sourceTargetName);
-            var destinationBuildPhase = getBuildPhaseByTargetName(destinationTargetName);
-
-            // remove all build-files from the destination-target
-            destinationBuildPhase.files.forEach(function(file, index) {
-                delete buildFileSection[file.value];
-                delete buildFileSection[file.value + '_comment'];
-            });
-
-            // reset destination build phase files.
-            destinationBuildPhase.files = [];
-
-            // create new "build-files" for Beta
-            sourceBuildPhase.files.forEach(function(file, index) {
-                var newID = project.generateUuid();
-                var buildFile = buildFileSection[file.value];
-                var adjusted = extend({}, file, {value: newID});
-
-                // add file to build section
-                buildFileSection[newID] = buildFile;
-
-                // add file to "beta" build phase
-                destinationBuildPhase.files.push(adjusted);
-            });
-        };
+glob("**/*.pbxproj", {follow: true}, function(error, files) {
+    if (files.length == 0) {
+        console.log("Unable to find any projects (*.xcodeproj)");
+        return;
     }
 
-    var syncer = new BuildPhaseSyncer();
-    syncer.sync('TargetSync', '"TargetSync Beta"');
+    console.log("Found the following xcode projects:");
+    console.log("");
 
-    var backupDirectory = path.dirname(filePath);
-    var backupFileName = path.basename(filePath) + '.orig';
-    var backupFilePath = path.join(backupDirectory, backupFileName);
+    files.forEach(function(file, index) {
+        console.log("    [%d] %s", (index+1), file);
+    });
 
-    fs.renameSync(filePath, backupFilePath);
-    fs.writeFileSync(filePath, project.writeSync());
+    console.log("");
+
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question('What project would you like to target? ', (answer) => {
+        var index = parseInt(answer);
+        if (index < 1 || index > files.length) {
+            console.log('Invalid value');
+        }
+        else {
+            sync(files[index-1]);
+        }
+
+        rl.close();
+    });
 });
+
+function sync(projectFilePath) {
+  var project = xcode.project(projectFilePath);
+  project.parse(function(error, data) {
+
+      function BuildPhaseSyncer() {
+          var buildFileSection = project.pbxBuildFileSection();
+          var buildFileReferenceSection = project.pbxFileReferenceSection();
+
+          function getBuildPhaseByTargetName(targetName) {
+              var key = project.findTargetKey(targetName);
+              var buildPhase = project.pbxSourcesBuildPhaseObj(key);
+
+              return buildPhase;
+          }
+
+          this.sync = function(sourceTargetName, destinationTargetName) {
+              var sourceBuildPhase = getBuildPhaseByTargetName(sourceTargetName);
+              var destinationBuildPhase = getBuildPhaseByTargetName(destinationTargetName);
+
+              // remove all build-files from the destination-target
+              destinationBuildPhase.files.forEach(function(file, index) {
+                  delete buildFileSection[file.value];
+                  delete buildFileSection[file.value + '_comment'];
+              });
+
+              // reset destination build phase files.
+              destinationBuildPhase.files = [];
+
+              // create new "build-files" for Beta
+              sourceBuildPhase.files.forEach(function(file, index) {
+                  var newID = project.generateUuid();
+                  var sourceBuildFile = buildFileSection[file.value];
+                  var newBuildPhaseFile = extend({}, file, {value: newID});
+
+                  // add file to build section (same file-ref but using a new id)
+                  buildFileSection[newID] = extend({}, sourceBuildFile);
+
+                  // add build-file reference to the destination build phase files.
+                  destinationBuildPhase.files.push(newBuildPhaseFile);
+              });
+          };
+      }
+
+      var syncer = new BuildPhaseSyncer();
+      syncer.sync('TargetSync', '"TargetSync Beta"');
+
+      var backupDirectory = path.dirname(projectFilePath);
+      var backupFileName = path.basename(projectFilePath) + '.orig';
+      var backupFilePath = path.join(backupDirectory, backupFileName);
+
+      fs.renameSync(projectFilePath, backupFilePath);
+      fs.writeFileSync(projectFilePath, project.writeSync());
+  });
+}
